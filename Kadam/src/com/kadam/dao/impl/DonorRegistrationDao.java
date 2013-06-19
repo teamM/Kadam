1d0cffb9884e004c6e34570a7bfcc22dea371484
vivek <daredevil.vivek@gmail.com>
2013-06-19 23:35:50 +0530

    Donor Registration module has been completed

--- Kadam/src/com/kadam/dao/impl/DonorRegistrationDao.java
+++ Kadam/src/com/kadam/dao/impl/DonorRegistrationDao.java
@@ -12,7 +12,7 @@ import com.kadam.vo.DonorRegistrationVO;
 
 public class DonorRegistrationDao {
 	
-	PreparedStatement addcolumn_statement=null,teachertable_statement,addteacher_statement,addaccount_statement,generatepassword_statement;
+	PreparedStatement statement;
 	Connection con;
 	ResultSet result;
 	public DonorRegistrationDao() throws KadamException, KadamBusinessException {
@@ -30,8 +30,28 @@ public class DonorRegistrationDao {
 	
 
 	
-	boolean donorRegistration(DonorRegistrationVO don_vo){
-		
+	public boolean donorRegistration(DonorRegistrationVO donor_reg_vo) throws KadamBusinessException{
+		try {
+			statement=con.prepareStatement("insert into master_donor (don_id,don_name,don_regdate,don_commitment ,don_address ,don_phone1,don_phone2,don_bday,don_anniv,don_email ,don_email2 ,don_workplace,don_designation,don_pan,don_comments) values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			statement.setDate(1, donor_reg_vo.getDonor_regdate());
+			statement.setString(2, donor_reg_vo.getDonor_commitment());
+			statement.setString(3, donor_reg_vo.getDonor_address());
+			statement.setInt(4, donor_reg_vo.getDonor_phone1());
+			statement.setInt(5, donor_reg_vo.getDonor_phone2());
+			statement.setDate(6, donor_reg_vo.getDonor_bdy());
+			statement.setDate(7, donor_reg_vo.getDonor_annvi());
+			statement.setString(8, donor_reg_vo.getDonor_email());
+			statement.setString(9, donor_reg_vo.getDonor_email2());
+			statement.setString(10, donor_reg_vo.getDonor_workplace());
+			statement.setString(12, donor_reg_vo.getDonor_designation());
+			statement.setString(13, donor_reg_vo.getDonor_pancard());
+			statement.setString(14, donor_reg_vo.getDonor_comments());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new KadamBusinessException(e+"Master donor information could not be inserted");
+		}
+
 		return false;
 	}
 
